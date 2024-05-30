f02e14adc0cb6d5a7bdd7619fb73e3468fd28679
unknown <chameauq@gmail.com>
2024-05-30 15:19:11 +0200

    Feat(main): change camera position

--- built/setup.js
+++ built/setup.js
@@ -1,7 +1,7 @@
 export const GLOBAL = {};
-GLOBAL.yaw = 0;
-GLOBAL.pitch = Math.PI * (-(1 / 5));
+GLOBAL.yaw = Math.PI * -(1 / 2);
+GLOBAL.pitch = Math.PI * -(1 / 5);
 GLOBAL.locked = false;
 GLOBAL.up = [0, 1, 0];
-GLOBAL.lookDirection = [0, 0, -1];
-GLOBAL.cameraPosition = [0, 7, 10];
+GLOBAL.lookDirection = [0, 0, -1]; //dont define, modify yaw and pitch
+GLOBAL.cameraPosition = [-10, 23, 15];
